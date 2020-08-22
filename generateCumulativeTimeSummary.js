c684257d91bf67370917363a990631ad772d082a
Jeffrey Fisher <jeff@slofish.io>
2020-08-22 13:58:31 -0700

    Refactor to make testing easier

---
+++ generateCumulativeTimeSummary.js
@@ -0,0 +1,78 @@
+function generateCumulativeTimeSummary(filterStartDate, filterEndDate, iCalendarData) {
+    var jcalData = ICAL.parse(iCalendarData);
+    var comp = new ICAL.Component(jcalData);
+    var vevents = comp.getAllSubcomponents('vevent');
+
+    let cumulativeDuration = {};
+
+    vevents.forEach((vevent) => {
+      var event = new ICAL.Event(vevent);
+      var summary = event.summary;
+      var eventStartDate = event.startDate.toJSDate();
+      var eventEndDate = event.endDate.toJSDate();
+      let duration = event.duration.toSeconds() / 60;
+
+      if (event.sequence) {
+        var expand = new ICAL.RecurExpansion({
+          component: vevent,
+          dtstart: vevent.getFirstPropertyValue('dtstart'),
+        });
+
+        let i = 0;
+        let max = 500;
+        let dates = [];
+        let now = new Date();
+        let next;
+        while ((next = expand.next())) {
+          eventStartDate = next.toJSDate();
+
+          if (i++ > max) {
+            console.error('too many dates');
+            break;
+          }
+          if (eventStartDate < filterStartDate) {
+            continue;
+          }
+          if (eventStartDate > filterEndDate) {
+            break;
+          }
+
+          console.log(event);
+          console.log(summary);
+          console.log('Duration: ', duration);
+          console.log(next.toJSDate());
+
+          if (cumulativeDuration.hasOwnProperty(summary)) {
+            cumulativeDuration[summary] += duration;
+          } else {
+            cumulativeDuration[summary] = duration;
+          }
+        }
+      } else {
+        if (eventStartDate < filterStartDate) {
+          return;
+        }
+        if (eventStartDate > filterEndDate) {
+          return;
+        }
+
+        console.log(event);
+        console.log(summary);
+        console.log('Duration: ', duration);
+        console.log(eventStartDate);
+
+        if (cumulativeDuration.hasOwnProperty(summary)) {
+          cumulativeDuration[summary] += duration;
+        } else {
+          cumulativeDuration[summary] = duration;
+        }
+
+        console.log('');
+      }
+
+      console.log('---------------------------');
+      console.log('');
+    });
+
+    console.log('Summary: ', cumulativeDuration);
+}
--- script.js
+++ script.js
@@ -1,89 +1,3 @@
-function diff_minutes(dt2, dt1) {
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000 / 60;
-  return Math.abs(Math.round(diff));
-}
-
-function generateCumulateTimeSummary(filterStartDate, filterEndDate, iCalendarData) {
-    var jcalData = ICAL.parse(iCalendarData);
-    var comp = new ICAL.Component(jcalData);
-    var vevents = comp.getAllSubcomponents('vevent');
-
-    let cumulativeDuration = {};
-
-    vevents.forEach((vevent) => {
-      var event = new ICAL.Event(vevent);
-      var summary = event.summary;
-      var eventStartDate = event.startDate.toJSDate();
-      var eventEndDate = event.endDate.toJSDate();
-      let duration = event.duration.toSeconds() / 60;
-
-      if (event.sequence) {
-        var expand = new ICAL.RecurExpansion({
-          component: vevent,
-          dtstart: vevent.getFirstPropertyValue('dtstart'),
-        });
-
-        let i = 0;
-        let max = 500;
-        let dates = [];
-        let now = new Date();
-        let next;
-        while ((next = expand.next())) {
-          eventStartDate = next.toJSDate();
-
-          if (i++ > max) {
-            console.error('too many dates');
-            break;
-          }
-          if (eventStartDate < filterStartDate) {
-            continue;
-          }
-          if (eventStartDate > filterEndDate) {
-            break;
-          }
-
-          console.log(event);
-          console.log(summary);
-          console.log('Duration: ', duration);
-          console.log(next.toJSDate());
-
-          if (cumulativeDuration.hasOwnProperty(summary)) {
-            cumulativeDuration[summary] += duration;
-          } else {
-            cumulativeDuration[summary] = duration;
-          }
-        }
-      } else {
-        if (eventStartDate < filterStartDate) {
-          return;
-        }
-        if (eventStartDate > filterEndDate) {
-          return;
-        }
-
-        console.log(event);
-        console.log(summary);
-        console.log('Duration: ', duration);
-        console.log(eventStartDate);
-
-        if (cumulativeDuration.hasOwnProperty(summary)) {
-          cumulativeDuration[summary] += duration;
-        } else {
-          cumulativeDuration[summary] = duration;
-        }
-
-        console.log('');
-      }
-
-      console.log('---------------------------');
-      console.log('');
-    });
-
-    console.log('Summary: ', cumulativeDuration);
-}
-
-
-document.getElementById('upload').addEventListener('change', readFileAsString);
 function readFileAsString() {
   var files = this.files;
   if (files.length === 0) {
@@ -118,7 +32,9 @@ function readFileAsString() {
     console.log('Chosen start date: ', chosenStartDate);
     console.log('Chosen end date: ', chosenEndDate);
 
-    generateCumulateTimeSummary(chosenStartDate, chosenEndDate, iCalendarData);
+    generateCumulativeTimeSummary(chosenStartDate, chosenEndDate, iCalendarData);
   };
   reader.readAsText(files[0]);
 }
+
+document.getElementById('upload').addEventListener('change', readFileAsString);
