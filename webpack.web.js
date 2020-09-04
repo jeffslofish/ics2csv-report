c06db2ed19fdc8e2e357c9d6fa22b857611316dd
Jeffrey Fisher <jeff@slofish.io>
2020-09-03 21:35:08 -0700

    Make web build put all needed files into dist-web

--- webpack.web.js
+++ webpack.web.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'static' }],
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist-web'),
     filename: 'ics2csv.js',
